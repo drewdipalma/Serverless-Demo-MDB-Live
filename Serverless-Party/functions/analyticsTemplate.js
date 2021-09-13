1ae472884067bce1e47783ce96ff9755da0ce6f5
Drew DiPalma <drew.dipalma@mongodb.com>
2021-09-13 16:10:10 -0400

    Update w/ Remote Config
    Update w/ Remote Config

--- Serverless-Party/functions/analyticsTemplate.js
+++
@@ -1,8 +0,0 @@
-exports = function(arg){
-  return {
-    from: "Mailgun Sandbox <postmaster@sandboxce5b6bcc8d0f41fea97ad1afba8b97b8.mailgun.org>",
-    to: "drew.dipalma@mongodb.com",
-    subject: "Hello w/ Attachment?",
-    content: "Hello Friend!"
-    }
-};
--- Serverless-Party/functions/textTemplate.js
+++
@@ -1,7 +0,0 @@
-exports = function(arg){
-  return {
-       body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-       from: '+16573124502',
-       to: '+19145840242'
-     }
-};
